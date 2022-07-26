e645fe2769116f21818a607f2159d366ffb83404
ajinkyaghude <ajinkya.ghude@gmail.com>
2022-07-26 13:54:16 +0000

    updated test file

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -8,3 +8,15 @@ async def test_mux(dut):
     """Test for mux2"""
 
     cocotb.log.info('##### CTB: Develop your test here ########')
+
+    inp12=2
+    sel =12
+    dut.sel.value = sel
+    dut.inp12.value = inp12
+
+    await Timer(2, units='ns')
+    
+    assert dut.out.value == inp12, "MUX is not working properly: {inp12} != {out}, expected value={EXP}".format(
+            inp5=int(dut.inp12.value), sel=int(dut.sel.value), out=int(dut.out.value), EXP=inp12)
+            
+
